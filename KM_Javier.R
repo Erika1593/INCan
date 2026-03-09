88482e7a1680e030ceb937ed4efa8b36dbf12730
Erika <60374107+Erika1593@users.noreply.github.com>
2026-03-09 16:27:37 -0600

    Update KM_Javier.R
    update, nicer plots

--- KM_Javier.R
+++ KM_Javier.R
@@ -19,42 +19,111 @@ TESIS_3N <- TESIS %>%
   filter(Inmunofenotipo == 4)
 
 ##SLE 
-SLE <- Surv(TESIS_3N$Tiempo_SLE, TESIS_3N$Super_libre_evento)  #Sustituir por D_DX_DEF para cálculo por días 
+SLE <- Surv(TESIS_3N$Tiempo_SLE, TESIS_3N$Super_libre_evento)  
 T_SLE <- survfit(SLE ~ 1, data = TESIS_3N, type = "kaplan-meier") 
 summary(T_SLE)
 
 
 #Gráfica SLE 
 
-ROJO <- ggsurvplot(fit = T_SLE, data = TESIS_3N, conf.int = T, palette = c("#ED0C0C"), conf.int.fill = c("#BFC9CA"), title = "SLE", 
-                   xlab = "meses", ylab = "SLE", legend.title = "Estimación", 
+SLEp <- ggsurvplot(fit = T_SLE, data = TESIS_3N, fun = "pct", conf.int = F, palette = c("#1F4E79"), size = 1.2, #conf.int.fill = c("#BFC9CA"), 
+                   title = "SLE", 
+                   break.time.by = 5, xlim = c(0,30), ylim = c(0,100), 
+                   xlab = "Meses", ylab = "Supervivencia (%)", legend.title = "Estimación", 
+                   ggtheme = theme_classic(base_size = 14, base_family = "Helvetica"), 
                    legend.labs = c("SLE")) 
 
+##ver gráfica
+SLEp$plot
+
+#personalización ggplot
+SLEp$plot <- SLEp$plot + 
+  scale_x_continuous(
+    breaks = seq(0, 30, 5),
+    limits = c(0, 30),
+    expand = c(0, 0)
+  ) +
+  scale_y_continuous(
+    breaks = seq(0, 100, 20),
+    limits = c(0, 100),
+    expand = c(0, 0)
+  ) +
+  geom_vline(
+    xintercept = 24,
+    linetype = "dashed",
+    color = "grey30",
+    linewidth = 0.8
+  ) +
+  theme(
+    axis.line = element_line(linewidth = 0.8),
+    axis.text = element_text(color = "black"),
+    axis.title = element_text(face = "bold"),
+    plot.margin = margin(10, 15, 10, 10)
+  )
+
+
+print(SLEp)
 
 
 ##Guardar imagen 
-ROJO$plot
-ggsave("ROJO.tiff", ROJO$plot, dpi = 300)
+SLEp$plot
+ggsave("SLEP.tiff", SLEp$plot, dpi = 300)
 
 ##SLE Estratificado 
 
 TESIS_3N$Estatus <- factor(TESIS_3N$Estatus, levels = c(0,1),
-                          labels = c("Negativo", "Positivo"))
+                           labels = c("Negativo", "Positivo"))
 
-SLE <- Surv(TESIS_3N$Tiempo_SLE, TESIS_3N$Super_libre_evento)  #Sustituir por D_DX_DEF para cálculo por días 
+SLE <- Surv(TESIS_3N$Tiempo_SLE, TESIS_3N$Super_libre_evento)  
 T_SLE_E <- survfit(SLE ~ Estatus, data = TESIS_3N, type = "kaplan-meier") 
 summary(T_SLE_E)
 
 
 #Gráfica SLE Estratificado
-ROSA <- ggsurvplot(fit = T_SLE_E, data = TESIS_3N, conf.int = T, palette = c("#ff5aa4", "#2E86C1"), conf.int.fill = c("#BFC9CA"), title = "SLE", 
-                   xlab = "meses", ylab = "SLE", legend.title = "Estimación", 
+SLE2 <- ggsurvplot(fit = T_SLE_E, data = TESIS_3N, fun = "pct", conf.int = F, palette = c("#FC2C03", "#1F4E79"), size = 1.2, #conf.int.fill = c("#BFC9CA"), 
+                   title = "SLE", 
+                   break.time.by = 5, xlim = c(0,30), ylim = c(0,100), 
+                   xlab = "Meses", ylab = "Supervivencia (%)", legend.title = "Estimación", 
+                   ggtheme = theme_classic(base_size = 14, base_family = "Helvetica"), 
                    legend.labs = c("irAE presente", "irAE ausente")) 
+
+SLE2$plot
 
 
 ##Guardar imagen 
-ROSA$plot
-ggsave("ROSA.tiff", ROSA$plot, dpi = 300)
+SLE2$plot
+
+
+#personalización ggplot
+SLE2$plot <- SLE2$plot + 
+  scale_x_continuous(
+    breaks = seq(0, 30, 5),
+    limits = c(0, 30),
+    expand = c(0, 0)
+  ) +
+  scale_y_continuous(
+    breaks = seq(0, 100, 20),
+    limits = c(0, 100),
+    expand = c(0, 0)
+  ) +
+  geom_vline(
+    xintercept = 24,
+    linetype = "dashed",
+    color = "grey30",
+    linewidth = 0.8
+  ) +
+  theme(
+    axis.line = element_line(linewidth = 0.8),
+    axis.text = element_text(color = "black"),
+    axis.title = element_text(face = "bold"),
+    plot.margin = margin(10, 15, 10, 10)
+  )
+
+
+print(SLE2)
+
+##GUARDAR
+ggsave("SLE2.tiff", SLE2$plot, dpi = 300)
 
 
 ##SG
@@ -65,15 +134,47 @@ summary(T_SG)
 
 #Gráfica SG
 
-AZUL <- ggsurvplot(fit = T_SG, data = TESIS_3N, conf.int = T, palette = c("#100CED"), conf.int.fill = c("#BFC9CA"), title = "SG", 
-                   xlab = "meses", ylab = "SG", legend.title = "Estimación", 
+SGp <- ggsurvplot(fit = T_SG, data = TESIS_3N, fun = "pct", conf.int = F, palette = c("#1F4E79"), size = 1.2, #conf.int.fill = c("#BFC9CA"), 
+                  title = "SG",
+                  break.time.by = 5, xlim = c(0,30), ylim = c(0,100),
+                   xlab = "Meses", ylab = "Supervivencia (%)", legend.title = "Estimación", 
+                  ggtheme = theme_classic(base_size = 14, base_family = "Helvetica"), 
                    legend.labs = c("SG")) 
 
+SGp$plot
+
+#personalización ggplot
+SGp$plot <- SGp$plot + 
+  scale_x_continuous(
+    breaks = seq(0, 30, 5),
+    limits = c(0, 30),
+    expand = c(0, 0)
+  ) +
+  scale_y_continuous(
+    breaks = seq(0, 100, 20),
+    limits = c(0, 100),
+    expand = c(0, 0)
+  ) +
+  geom_vline(
+    xintercept = 24,
+    linetype = "dashed",
+    color = "grey30",
+    linewidth = 0.8
+  ) +
+  theme(
+    axis.line = element_line(linewidth = 0.8),
+    axis.text = element_text(color = "black"),
+    axis.title = element_text(face = "bold"),
+    plot.margin = margin(10, 15, 10, 10)
+  )
+
+
+print(SGp)
 
 
 ##Guardar imagen 
-AZUL$plot
-ggsave("AZUL.tiff", AZUL$plot, dpi = 300)
+SGp$plot
+ggsave("SGp.tiff", SGp$plot, dpi = 300)
 
 
 ##SG Estratificado 
@@ -86,15 +187,49 @@ T_SG_E <- survfit(SG ~ Estatus, data = TESIS_3N, type = "kaplan-meier")
 summary(T_SG_E)
 
 
-#Gráfica SLE Estratificado
-AZULC <- ggsurvplot(fit = T_SG_E, data = TESIS_3N, conf.int = T, palette = c("#ff5aa4", "#2E86C1"), conf.int.fill = c("#BFC9CA"), title = "SG", 
-                   xlab = "meses", ylab = "SG", legend.title = "Estimación", 
-                   legend.labs = c("irAE presente", "irAE ausente")) 
+#Gráfica SG Estratificado
+SG2 <- ggsurvplot(fit = T_SG_E, data = TESIS_3N, fun = "pct", conf.int = F, palette = c("#FC2C03", "#1F4E79"),  size = 1.2, #conf.int.fill = c("#BFC9CA"), 
+                  title = "SG", 
+                    xlab = "Meses", ylab = "Supervivencia (%)", legend.title = "Estimación", 
+                  ggtheme = theme_classic(base_size = 14, base_family = "Helvetica"),
+                    legend.labs = c("irAE presente", "irAE ausente")) 
+
+
+SG2$plot
+
+#personalización ggplot
+SG2$plot <- SG2$plot + 
+  scale_x_continuous(
+    breaks = seq(0, 30, 5),
+    limits = c(0, 30),
+    expand = c(0, 0)
+  ) +
+  scale_y_continuous(
+    breaks = seq(0, 100, 20),
+    limits = c(0, 100),
+    expand = c(0, 0)
+  ) +
+  geom_vline(
+    xintercept = 24,
+    linetype = "dashed",
+    color = "grey30",
+    linewidth = 0.8
+  ) +
+  theme(
+    axis.line = element_line(linewidth = 0.8),
+    axis.text = element_text(color = "black"),
+    axis.title = element_text(face = "bold"),
+    plot.margin = margin(10, 15, 10, 10)
+  )
+
+
+print(SG2)
+
 
 
 ##Guardar imagen 
-AZULC$plot
-ggsave("AZULC.tiff", AZULC$plot, dpi = 300)
+SG2$plot
+ggsave("SG2.tiff", SG2$plot, dpi = 300)
 
 
 ##Filtrar por cirugía 
@@ -117,87 +252,101 @@ summary(T_SLE_p)
 
 
 #Gráfica SLE Estratificado por pCR 
-ROSA_pcr <- ggsurvplot(fit = T_SLE_p, data = TESIS_C, conf.int = T, palette = c("#ff5aa4", "#2E86C1"), conf.int.fill = c("#BFC9CA"), title = "SLE", 
-                   xlab = "meses", ylab = "SLE", legend.title = "Estimación", 
-                   legend.labs = c("pCR ausente", "pCR presente")) 
+SLE_pcr <- ggsurvplot(fit = T_SLE_p, data = TESIS_C, fun = "pct", conf.int = F, palette = c("#FC2C03", "#1F4E79"), size = 1.2,  #conf.int.fill = c("#BFC9CA"), title = "SLE", 
+                       xlab = "Meses", ylab = "Supervivencia (%)", legend.title = "Estimación", 
+                       ggtheme = theme_classic(base_size = 14, base_family = "Helvetica"),
+                       legend.labs = c("pCR ausente", "pCR presente")) 
+
+
+
+SLE_pcr$plot
+
+#personalización ggplot
+SLE_pcr$plot <- SLE_pcr$plot + 
+  scale_x_continuous(
+    breaks = seq(0, 30, 5),
+    limits = c(0, 30),
+    expand = c(0, 0)
+  ) +
+  scale_y_continuous(
+    breaks = seq(0, 100, 20),
+    limits = c(0, 100),
+    expand = c(0, 0)
+  ) +
+  geom_vline(
+    xintercept = 24,
+    linetype = "dashed",
+    color = "grey30",
+    linewidth = 0.8
+  ) +
+  theme(
+    axis.line = element_line(linewidth = 0.8),
+    axis.text = element_text(color = "black"),
+    axis.title = element_text(face = "bold"),
+    plot.margin = margin(10, 15, 10, 10)
+  )
+
+
+print(SLE_pcr)
+
 
 
 ##Guardar imagen 
-ROSA_pcr$plot
-ggsave("ROSA.tiff", ROSA_pcr$plot, dpi = 300)
+SLE_pcr$plot
+ggsave("SLE.tiff", SLE_pcr$plot, dpi = 300)
 
 
 ##SG Estratificado por pCR 
-
-TESIS_C$pCR <- factor(TESIS_C$pCR, levels = c(0,1),
-                           labels = c("Negativo", "Positivo"))
-
-SG_P <- Surv(TESIS_C$Tiempo_SG, TESIS_C$Vivo_muerto) 
-T_SG_P <- survfit(SG_P ~ pCR, data = TESIS_C, type = "kaplan-meier") 
-summary(T_SG_P)
-
-
-#Gráfica SLE Estratificado
-AZULC <- ggsurvplot(fit = T_SG_P, data = TESIS_C, conf.int = T, palette = c("#ff5aa4", "#2E86C1"), conf.int.fill = c("#BFC9CA"), title = "SG", 
-                    xlab = "meses", ylab = "SG", legend.title = "Estimación", 
-                    legend.labs = c("pCR ausente", "pCR presente")) 
-
-
-##Guardar imagen 
-AZULC$plot
-ggsave("AZULC.tiff", AZULC$plot, dpi = 300)
-
-
-
-
-
-##SLE pCR
-
-SLE_p <- Surv(TESIS_C$Tiempo_SLE, TESIS_C$Super_libre_evento) 
-T_SLE_p <- survfit(SLE ~ 1, data = TESIS_C, type = "kaplan-meier") 
-summary(T_SLE_p)
 
 TESIS_C$pCR <- factor(TESIS_C$pCR, levels = c(0,1),
                       labels = c("Negativo", "Positivo"))
 
-SLE_p <- Surv(TESIS_C$Tiempo_SLE, TESIS_C$Super_libre_evento) 
-T_SLE_p <- survfit(SLE_p ~ pCR, data = TESIS_C, type = "kaplan-meier") 
-summary(T_SLE_p)
-
-
-
-#Gráfica SLE Estratificado por pCR 
-ROSA_pcr <- ggsurvplot(fit = T_SLE_p, data = TESIS_C, conf.int = T, palette = c("#ff5aa4", "#2E86C1"), conf.int.fill = c("#BFC9CA"), title = "SLE", 
-                   xlab = "meses", ylab = "SLE", legend.title = "Estimación", 
-                   legend.labs = c("pCR ausente", "pCR presente")) 
-
-
-##Guardar imagen 
-ROSA_pcr$plot
-ggsave("ROSA.tiff", ROSA_pcr$plot, dpi = 300)
-
-
-##SG Estratificado por pCR 
-
-TESIS_C$pCR <- factor(TESIS_C$pCR, levels = c(0,1),
-                           labels = c("Negativo", "Positivo"))
-
 SG_P <- Surv(TESIS_C$Tiempo_SG, TESIS_C$Vivo_muerto) 
 T_SG_P <- survfit(SG_P ~ pCR, data = TESIS_C, type = "kaplan-meier") 
 summary(T_SG_P)
 
 
-#Gráfica SG Estratificado
-AZULC <- ggsurvplot(fit = T_SG_P, data = TESIS_C, conf.int = T, palette = c("#ff5aa4", "#2E86C1"), conf.int.fill = c("#BFC9CA"), title = "SG", 
-                    xlab = "meses", ylab = "SG", legend.title = "Estimación", 
+#Gráfica SG pcr 
+SG_pcr <- ggsurvplot(fit = T_SG_P, data = TESIS_C, fun = "pct", conf.int = F, palette = c("#FC2C03", "#1F4E79"), size = 1.2,  #conf.int.fill = c("#BFC9CA"), title = "SG", 
+                    xlab = "Meses", ylab = "Supervivencia (%)", legend.title = "Estimación", 
+                    ggtheme = theme_classic(base_size = 14, base_family = "Helvetica"),
                     legend.labs = c("pCR ausente", "pCR presente")) 
 
 
+SG_pcr$plot
+
+#personalización ggplot
+SG_pcr$plot <- SG_pcr$plot + 
+  scale_x_continuous(
+    breaks = seq(0, 30, 5),
+    limits = c(0, 30),
+    expand = c(0, 0)
+  ) +
+  scale_y_continuous(
+    breaks = seq(0, 100, 20),
+    limits = c(0, 100),
+    expand = c(0, 0)
+  ) +
+  geom_vline(
+    xintercept = 24,
+    linetype = "dashed",
+    color = "grey30",
+    linewidth = 0.8
+  ) +
+  theme(
+    axis.line = element_line(linewidth = 0.8),
+    axis.text = element_text(color = "black"),
+    axis.title = element_text(face = "bold"),
+    plot.margin = margin(10, 15, 10, 10)
+  )
+
+
+print(SG_pcr)
+
+
 ##Guardar imagen 
-AZULC$plot
-ggsave("AZULC.tiff", AZULC$plot, dpi = 300)
-
-
+SG_pcr$plot
+ggsave("SG_pcr.tiff", SG_pcr$plot, dpi = 300)
 
 
 ##SLE BCR 
@@ -207,23 +356,54 @@ T_SLE_b <- survfit(SLE_b ~ 1, data = TESIS_C, type = "kaplan-meier")
 summary(T_SLE_b)
 
 TESIS_C$RCB_cat <- factor(TESIS_C$RCB_cat, levels = c(0,1),
-                      labels = c("2-3", "0-1"))
+                          labels = c("2-3", "0-1"))
 
 SLE_b <- Surv(TESIS_C$Tiempo_SLE, TESIS_C$Super_libre_evento) 
 T_SLE_b <- survfit(SLE_b ~ RCB_cat, data = TESIS_C, type = "kaplan-meier") 
 summary(T_SLE_b)
 
 
-#Gráfica SLE Estratificado por pCR 
-ROSA_rcb <- ggsurvplot(fit = T_SLE_b, data = TESIS_C, conf.int = T, palette = c("#ff5aa4", "#2E86C1"), conf.int.fill = c("#BFC9CA"), title = "SLE", 
-                       xlab = "meses", ylab = "SLE", legend.title = "Estimación", 
+#Gráfica SLE Estratificado por rcb 
+SLE_rcb <- ggsurvplot(fit = T_SLE_b, data = TESIS_C, fun = "pct", conf.int = F, palette = c("#FC2C03", "#1F4E79"), size = 1.2, #conf.int.fill = c("#BFC9CA"), title = "SLE", 
+                       xlab = "Meses", ylab = "Supervivencia (%)", legend.title = "Estimación", 
+                       ggtheme = theme_classic(base_size = 14, base_family = "Helvetica"),
                        legend.labs = c("RCB 2-3", "RCB 0-1")) 
 
 
+SLE_rcb$plot
+
+#personalización ggplot
+SLE_rcb$plot <- SLE_rcb$plot + 
+  scale_x_continuous(
+    breaks = seq(0, 30, 5),
+    limits = c(0, 30),
+    expand = c(0, 0)
+  ) +
+  scale_y_continuous(
+    breaks = seq(0, 100, 20),
+    limits = c(0, 100),
+    expand = c(0, 0)
+  ) +
+  geom_vline(
+    xintercept = 24,
+    linetype = "dashed",
+    color = "grey30",
+    linewidth = 0.8
+  ) +
+  theme(
+    axis.line = element_line(linewidth = 0.8),
+    axis.text = element_text(color = "black"),
+    axis.title = element_text(face = "bold"),
+    plot.margin = margin(10, 15, 10, 10)
+  )
+
+
+print(SLE_rcb)
+
 
 ##Guardar imagen 
-ROSA_rcb$plot
-ggsave("ROSA.tiff", ROSA_pcr$plot, dpi = 300)
+SLE_rcb$plot
+ggsave("SLE_rcb.tiff", SLE_pcr$plot, dpi = 300)
 
 
 
@@ -236,13 +416,46 @@ T_SG_b <- survfit(SG_b ~ RCB_cat, data = TESIS_C, type = "kaplan-meier")
 summary(T_SG_b)
 
 
-#Gráfica SG Estratificado
-AZULC <- ggsurvplot(fit = T_SG_b, data = TESIS_C, conf.int = T, palette = c("#ff5aa4", "#2E86C1"), conf.int.fill = c("#BFC9CA"), title = "SG", 
-                    xlab = "meses", ylab = "SG", legend.title = "Estimación", 
+#Gráfica SG Estratificado por rcb
+SG_rcb <- ggsurvplot(fit = T_SG_b, data = TESIS_C, fun = "pct", conf.int = F, palette = c("#FC2C03", "#1F4E79"), size = 1.2,#conf.int.fill = c("#BFC9CA"), title = "SG", 
+                    xlab = "Meses", ylab = "Supervivencia (%)", legend.title = "Estimación", 
+                    ggtheme = theme_classic(base_size = 14, base_family = "Helvetica"),
                     legend.labs = c("RCB 2-3", "RCB 0-1")) 
 
 
-##Guardar imagen 
-AZULC$plot
-ggsave("AZULC.tiff", AZULC$plot, dpi = 300)
 
+SG_rcb$plot
+
+#personalización ggplot
+SG_rcb$plot <- SG_rcb$plot + 
+  scale_x_continuous(
+    breaks = seq(0, 30, 5),
+    limits = c(0, 30),
+    expand = c(0, 0)
+  ) +
+  scale_y_continuous(
+    breaks = seq(0, 100, 20),
+    limits = c(0, 100),
+    expand = c(0, 0)
+  ) +
+  geom_vline(
+    xintercept = 24,
+    linetype = "dashed",
+    color = "grey30",
+    linewidth = 0.8
+  ) +
+  theme(
+    axis.line = element_line(linewidth = 0.8),
+    axis.text = element_text(color = "black"),
+    axis.title = element_text(face = "bold"),
+    plot.margin = margin(10, 15, 10, 10)
+  )
+
+
+print(SG_rcb)
+
+
+
+##Guardar imagen 
+SG_rcb$plot
+ggsave("SG_rcb.tiff", SG_rcb$plot, dpi = 300)
